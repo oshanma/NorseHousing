2762a9462490bc4b08c926e9004819f93409311c
Oshan Maharjan <oshanmaharjan23@gmail.com>
2025-04-29 22:44:17 -0400

    Login UI update

--- app/src/main/java/edu/nku/classapp/Login.kt
+++ app/src/main/java/edu/nku/classapp/Login.kt
@@ -5,9 +5,9 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.ui.input.key.Key.Companion.Home
 import com.google.firebase.auth.FirebaseAuth
 import edu.nku.classapp.R
 import edu.nku.classapp.home
@@ -48,5 +48,11 @@ class Login : AppCompatActivity() {
                     }
             }
         })
+        findViewById<TextView>(R.id.textRegisterRedirect).setOnClickListener {
+            val intent = Intent(this, Register::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
--- app/src/main/java/edu/nku/classapp/register.kt
+++ app/src/main/java/edu/nku/classapp/register.kt
@@ -5,6 +5,7 @@ import android.os.Bundle
 import android.view.View
 import android.widget.Button
 import android.widget.EditText
+import android.widget.TextView
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
@@ -46,5 +47,12 @@ class Register : AppCompatActivity() {
                     }
             }
         })
+        findViewById<TextView>(R.id.textLoginRedirect).setOnClickListener {
+            val intent = Intent(this, Login::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
 }
